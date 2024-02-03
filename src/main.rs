e2df75e6d2ccf8f2649b73cf69af1ea607222563
ZeD <ledung.itsme@gmail.com>
2024-02-03 21:06:08 +0700

    feat: add sample file, add compression logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+extern crate flate2;
+
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::env::args;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::process::exit;
+use std::time::Instant;
+
+fn main() -> Result<(), std::io::Error> {
+    if args().len() != 3 {
+        eprintln!("Usage: `source` `target`");
+        exit(1);
+    }
+
+    let input_file = args().nth(1).unwrap();
+    let output_file = args().nth(2).unwrap();
+
+    let mut input = BufReader::new(File::open(input_file).unwrap());
+    let output = File::create(output_file).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::best());
+    let start = Instant::now();
+
+    copy(&mut input, &mut encoder)?;
+    let output = encoder.finish()?;
+
+    println!("Source len: {:?}", input.get_ref().metadata()?.len());
+
+    println!("Target len: {:?}", output.metadata()?.len());
+    println!("Elapsed: {:?}", start.elapsed());
+
+    Ok(())
 }
